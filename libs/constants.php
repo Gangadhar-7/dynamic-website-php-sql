6cbbce4f9511a88c82e752971620af93b0f80d6c
Gangadhar_matta <102216404+Gangadhar-7@users.noreply.github.com>
2023-12-31 00:56:10 +0530

    Update constants.php

--- libs/constants.php
+++ libs/constants.php
@@ -9,7 +9,7 @@ define('SITE_DIR', '/dynamic-website/');
 define('DB_PREFIX', 'mp_');
 
 define('DB_DRIVER', 'mysql');
-define('DB_HOST', 'database-1.c7vq7maqswxz.us-east-1.rds.amazonaws.com');
+define('DB_HOST', 'projectrds.c7vq7maqswxz.us-east-1.rds.amazonaws.com');
 define('DB_HOST_USERNAME', 'admin');                                                                                                                                   
 define('DB_HOST_PASSWORD', 'admin123');
 define('DB_DATABASE', 'simple_website');
