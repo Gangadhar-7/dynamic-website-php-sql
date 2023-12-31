976e5a8853be189706a77080c21c664670e467ba
Gangadhar_matta <102216404+Gangadhar-7@users.noreply.github.com>
2023-12-31 20:06:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,10 @@ include("header.php");
 ?>
 <div class="row main-row">
     <div class="9u">
+        <section class="left-content">
+            <h2><?php echo stripslashes($pageDetails["page_title"]); ?></h2>
+            <?php echo stripslashes($pageDetails["page_desc"]); ?>
+        </section>
     
     </div>
 
